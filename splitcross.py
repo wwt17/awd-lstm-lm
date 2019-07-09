ce80c43d43062d4b7486f8c63b7d7a360ec93db4
Wentao Wang <wwt10@163.com>
2019-07-09 18:16:49 +0800

    change splitcross: replace view by reshape

--- splitcross.py
+++ splitcross.py
@@ -63,7 +63,7 @@ class SplitCrossEntropyLoss(nn.Module):
                 # Adding is equivalent to multiplication in log space
                 head_entropy = (softmaxed_head_res[:, -idx]).contiguous()
                 tail_entropy = torch.nn.functional.log_softmax(tail_res, dim=-1)
-                results.append(head_entropy.view(-1, 1) + tail_entropy)
+                results.append(head_entropy.reshape(-1, 1) + tail_entropy)
 
         if len(results) > 1:
             return torch.cat(results, dim=1)
@@ -76,7 +76,7 @@ class SplitCrossEntropyLoss(nn.Module):
 
         # Determine to which split each element belongs (for each start split value, add 1 if equal or greater)
         # This method appears slower at least for WT-103 values for approx softmax
-        #masks = [(targets >= self.splits[idx]).view(1, -1) for idx in range(1, self.nsplits)]
+        #masks = [(targets >= self.splits[idx]).reshape(1, -1) for idx in range(1, self.nsplits)]
         #mask = torch.sum(torch.cat(masks, dim=0), dim=0)
         ###
         # This is equally fast for smaller splits as method below but scales linearly
@@ -100,7 +100,7 @@ class SplitCrossEntropyLoss(nn.Module):
             # Are you in our split?
             tmp_mask = mask == idx
             split_targets.append(torch.masked_select(targets, tmp_mask))
-            split_hiddens.append(hiddens.masked_select(tmp_mask.unsqueeze(1).expand_as(hiddens)).view(-1, hiddens.size(1)))
+            split_hiddens.append(hiddens.masked_select(tmp_mask.unsqueeze(1).expand_as(hiddens)).reshape(-1, hiddens.size(1)))
         return split_targets, split_hiddens
 
     def forward(self, weight, bias, hiddens, targets, verbose=False):
@@ -110,7 +110,7 @@ class SplitCrossEntropyLoss(nn.Module):
             print()
 
         total_loss = None
-        if len(hiddens.size()) > 2: hiddens = hiddens.view(-1, hiddens.size(2))
+        if len(hiddens.size()) > 2: hiddens = hiddens.reshape(-1, hiddens.size(2))
 
         split_targets, split_hiddens = self.split_on_targets(hiddens, targets)
 
@@ -134,18 +134,17 @@ class SplitCrossEntropyLoss(nn.Module):
             self.stats[0].append(combo.size()[0] * head_weight.size()[0])
 
         running_offset = 0
+        total_loss = 0.
         for idx in range(self.nsplits):
             # If there are no targets for this split, continue
             if len(split_targets[idx]) == 0: continue
 
+            softmaxed_head_res = softmaxed_all_head_res[running_offset:running_offset + len(split_hiddens[idx])]
             # For those targets in the head (idx == 0) we only need to return their loss
             if idx == 0:
-                softmaxed_head_res = softmaxed_all_head_res[running_offset:running_offset + len(split_hiddens[idx])]
-                entropy = -torch.gather(softmaxed_head_res, dim=1, index=split_targets[idx].view(-1, 1))
+                entropy = -torch.gather(softmaxed_head_res, dim=1, index=split_targets[idx].reshape(-1, 1))
             # If the target is in one of the splits, the probability is the p(tombstone) * p(word within tombstone)
             else:
-                softmaxed_head_res = softmaxed_all_head_res[running_offset:running_offset + len(split_hiddens[idx])]
-
                 if self.verbose or verbose:
                     start, end = self.splits[idx], self.splits[idx + 1]
                     tail_weight = weight[start:end]
@@ -158,13 +157,13 @@ class SplitCrossEntropyLoss(nn.Module):
                 # Adding is equivalent to multiplication in log space
                 head_entropy = softmaxed_head_res[:, -idx]
                 # All indices are shifted - if the first split handles [0,...,499] then the 500th in the second split will be 0 indexed
-                indices = (split_targets[idx] - self.splits[idx]).view(-1, 1)
+                indices = (split_targets[idx] - self.splits[idx]).reshape(-1, 1)
                 # Warning: if you don't squeeze, you get an N x 1 return, which acts oddly with broadcasting
                 tail_entropy = torch.gather(torch.nn.functional.log_softmax(tail_res, dim=-1), dim=1, index=indices).squeeze()
                 entropy = -(head_entropy + tail_entropy)
             ###
             running_offset += len(split_hiddens[idx])
-            total_loss = entropy.float().sum() if total_loss is None else total_loss + entropy.float().sum()
+            total_loss = total_loss + entropy.float().sum()
 
         return (total_loss / len(targets)).type_as(weight)
 
