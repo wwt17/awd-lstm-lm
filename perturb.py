dfbaeb8517ba1b87bfa330822c9b9cb006297c91
Wentao Wang <wwt10@163.com>
2019-07-25 15:10:12 +0800

    merge context_size.py into perturb.py

--- perturb.py
+++ perturb.py
@@ -1,7 +1,7 @@
 """Evaluate a language model on perturbed input in the restricted context setting."""
 
 import argparse
-import time, os
+import time, os, sys
 import math
 import numpy as np
 import pickle
@@ -14,10 +14,11 @@ import data
 import model
 
 from utils_context import batchify_context, get_context_batch, get_vocab_all_pos
+from gpt2_decoder import GPT2Decoder
 # this is not a good practice, but making an exception in this case
 from perturbations import *
 
-parser = argparse.ArgumentParser(description='Test language model on perturbed inputs')
+parser = argparse.ArgumentParser(description='Test language model on perturbed inputs in the restricted context setting')
 parser.add_argument('--data', type=str, default='data/wikitext-103',
                     help='location of the data corpus')
 parser.add_argument('--checkpoint', type=str, default='./WT2.pt',
@@ -36,12 +37,12 @@ parser.add_argument('--batch_size', type=int, default=50, metavar='N',
 parser.add_argument('--seq_len', type=int, default=300,
                     help='sequence length')
 parser.add_argument('--max_seq_len', type=int, default=1000,
-                    help='max context size')
+                    help='Maximum possible sequence length to make all experiments start at the same example i.e. skip the same number of tokens at the start')
 parser.add_argument('--freq', type=int, default=108,
                     help='Frequent words cut-off, all words with corpus count > 800')
 parser.add_argument('--rare', type=int, default=1986,
                     help='Rare words cut-off, all words with corpus count < 50')
-parser.add_argument('--func', type=str,
+parser.add_argument('--func', type=str, default='',
                     help='random_drop_many, drop_pos, keep_pos, shuffle, shuffle_within_spans, reverse, reverse_within_spans, replace_target, replace_target_with_nearby_token, drop_target, replace_with_rand_seq',
                     choices=['random_drop_many', 'drop_pos', 'keep_pos', 'shuffle', 'shuffle_within_spans', 'reverse', 'reverse_within_spans', 'replace_target', 'replace_target_with_nearby_token', 'drop_target', 'replace_with_rand_seq'])
 parser.add_argument('--span', type=int, default=20,
@@ -73,15 +74,21 @@ start = time.time()
 model, criterion, optimizer = torch.load(args.checkpoint)
 print('[{:.1f} s]'.format(time.time() - start))
 
+is_GPT2 = isinstance(model, GPT2Decoder)
+
 if args.cuda:
     model.cuda()
 else:
     model.cpu()
 
+output_layer = model.decoder.output_layer if is_GPT2 else model.decoder
+def criterion_fn(output, targets):
+    return criterion(output_layer.weight, output_layer.bias, output.reshape(-1, output.size(-1)), targets.reshape(-1))
+
 corpus = data.prepare_corpus(args.data)
 
 pos_datafile = os.path.dirname(args.data if args.data.endswith('/') else args.data+'/')+'_pos/'
-print(pos_datafile)
+print('try POS data file: {}'.format(pos_datafile))
 if os.path.exists(pos_datafile):
     pos_corpus = data.Corpus(pos_datafile)
     print('Built pos corpus')
@@ -93,28 +100,29 @@ print('On {} set!!!'.format(args.stage))
 data_ = batchify_context(getattr(corpus, args.stage), args.batch_size)
 if pos_corpus is not None:
     pos_data = batchify_context(getattr(pos_corpus, args.stage), args.batch_size)
+    pos_dict = pos_corpus.dictionary
 else:
     pos_data = None
+    pos_dict = None
+
 if args.cuda:
     data_ = data_.cuda()
     if pos_corpus is not None:
         pos_data = pos_data.cuda()
 print('Made batches.')
 
-def evaluate(data_source, pdata_source, boundary, func, args, pos2vocab):
+def evaluate(data_source, pdata_source, perturb_fn, args):
     # Turn on evaluation mode which disables dropout.
     model.eval()
 
     total_loss = 0.
     n = 0
-    total_len = 0
     all_losses = []
 
-    if pos_corpus is not None:
-        pos_dict = pos_corpus.dictionary
-    else:
-        pos_dict = None
-
+    # Number of batches excludes the last seq_len tokens as start of context, since the target index would lie out of bounds.
+    # Skip examples at the beginning to ensure the first target is the same for each experiment.
+    # All experiments then operate on the same examples.
+    # Select the max_seq_len as the largest context size you'd like to test.
     nbatches = (data_source.size(0) - args.seq_len) // args.batch_size
     examples_to_ignore = args.max_seq_len - args.seq_len
     print('Number of examples to ignore: {}'.format(examples_to_ignore))
@@ -123,37 +131,43 @@ def evaluate(data_source, pdata_source, boundary, func, args, pos2vocab):
     cross_entropy = nn.CrossEntropyLoss(reduction='none')
 
     with torch.no_grad():
+        # when using your own LM, ensure the init hidden function exists!
         init_hidden = model.init_hidden(args.batch_size)
         t = tqdm(range(batches_to_ignore, nbatches))
         for i in t:
             data, targets = get_context_batch(data_source, i, args.batch_size, args.seq_len)
             if n == 0:
                 print('First word: {}'.format(corpus.dictionary.idx2word[data.data[-1, 0]]))
-            pdata, ptargets = get_context_batch(pdata_source, i, args.batch_size, args.seq_len)
-            data = perturb_data(data.transpose(1, 0), pdata.transpose(1, 0), boundary, func, args, pos_dict, targets[-1], ptargets[-1], pos2vocab)
+            pdata, ptargets = get_context_batch(pdata_source, i, args.batch_size, args.seq_len) if pdata_source is not None else (None, None)
+            data = perturb_fn(
+                data.transpose(1, 0), pdata.transpose(1, 0) if pdata is not None else None,
+                targets[-1], ptargets[-1] if ptargets is not None else None,
+                args)
 
             output, _ = model(data, init_hidden)
-            total_len += data.shape[0] * data.shape[1]
-
-            output = output.reshape(*(data.size() + output.size()[-1:]))
-            logits = model.decoder(output)
-            CELoss = cross_entropy(logits.transpose(1, 2), targets)
-            losses = CELoss[-1]
-            all_losses += losses.tolist()
-            loss = torch.sum(losses)
-            total_loss += loss.item()
+            loss = criterion_fn(output[-1:], targets[-1:])
+            total_loss += targets.size(1) * loss.item()
             n += targets.size(1)
-            mean_loss = loss / targets.size(1)
-            t.set_postfix_str('loss={:.6f} ppl={:.3f}'.format(mean_loss, math.exp(mean_loss)))
+            t.set_postfix_str('loss={:8.6f} ppl={:7.3f}'.format(loss, math.exp(loss)))
 
-        print('Last word: {}' % (corpus.dictionary.idx2word[data.data[-1, -1]]))
-        print('total: {}'.format(n))
-        print('=' * 89)
-        print('Average Sequence Length post changes  {:.4f}'.format(total_len / n))
-        print('=' * 89)
+        print('Last word: {}'.format(corpus.dictionary.idx2word[data[-1, -1]]))
+        print('Total: {}'.format(n))
 
     return total_loss / n, all_losses
 
+if not args.func:
+    loss, all_losses = evaluate(
+        data_, pos_data,
+        lambda data, pdata, targets, ptargets, args: data,
+        args)
+    print('seq_len: {}, loss: {}, ppl: {}'.format(args.seq_len, loss, math.exp(loss)))
+
+    if False:
+        with open(os.path.join(args.logdir, 'per_token_scores_{}_{}'.format(args.stage, args.seq_len)), 'wb') as f:
+            pickle.dump(args.seq_len, f)
+            pickle.dump(all_losses, f)
+
+    sys.exit(0)
 
 def perturb_data(data, pdata, boundary, func, args, pos_dict, targets, ptargets, pos2vocab):
     if 'pos' in args.func:
@@ -195,7 +209,13 @@ print(loop_range)
 for boundary in loop_range:
     log_msg, cfunc, res_label = pick_perturbation(args, boundary)
     print(log_msg)
-    loss, all_losses = evaluate(data_, pos_data, boundary, cfunc, args, pos2vocab)
+    loss, all_losses = evaluate(
+        data_, pos_data,
+        lambda data, pdata, targets, ptargets, args: perturb_data(
+            data, pdata,
+            boundary, cfunc, args, pos_dict,
+            targets, ptargets, pos2vocab),
+        args)
     print('{} loss: {}, ppl: {}'.format(res_label, loss, math.exp(loss)))
 
     with open(os.path.join(args.logdir, prefix+str(boundary)), 'wb') as f:
